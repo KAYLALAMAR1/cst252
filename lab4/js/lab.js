6217a9748589898dec3c4643ffcdccec9e152bf2
KAYLALAMAR1 <70286147+KAYLALAMAR1@users.noreply.github.com>
2021-02-09 18:12:48 -0800

    New Lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,12 @@
+//Gather wood from nearby trees
+//Open 3x3 crafting grid in inventory
+//Make wooden planks by placing wooden block in center of grid
+//Stack 2 wooden planks in grid to make sticks
+//Use 3 wooden planks and 2 sticks in grid to make wooden axe
+//Gather more wood from nearby trees.
+//Continue converting wood to wooden planks. At least 22 planks.
+//Use 6 wooden planks to make door
+//Find desirable location for house
+//Use wooden planks to build walls and roof. Walls should be at least 3 blocks tall.
+//Leave a hole in one wall. Hole should be 2 blocks tall
+//Place door in hole
