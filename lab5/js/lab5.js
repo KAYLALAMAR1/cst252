06b67d593da64b71c9a5c2661c8ff69fbf77c983
KAYLALAMAR1 <70286147+KAYLALAMAR1@users.noreply.github.com>
2021-02-13 20:52:22 -0800

    New Lab 5

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/**
+ * Author:    Kayla Lamar <klamar@csumb.edu>
+ * Created:   13 February
+ *
+ * License: Public Domain
+ **/
+
+// Define Variables
+make = "Nissan";
+model = "Quest";
+color = "Coffee";
+year = 2002;
+
+// calculate
+age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
