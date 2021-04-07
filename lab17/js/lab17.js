ffb7ff436be6647abef5179d7a8246527157ec55
KAYLALAMAR1 <70286147+KAYLALAMAR1@users.noreply.github.com>
2021-04-07 13:23:37 -0700

    new lab 17

---
+++ lab17/js/lab17.js
@@ -0,0 +1,19 @@
+/**
+ * Author:    Kayla Lamar <klamar@csumb.edu>
+ * Created:   7 April
+ *
+ * License: Public Domain
+ **/
+
+$("input:submit").click(function(){
+  console.log($("myinput").val());
+  $("#output").html($("input:text").val());
+})
+
+$("#challenge-button1").click(function(){
+  $("#challenge-button2").toggleClass("fluffy");
+	});
+
+  $("#challenge-button2").click(function(){
+    $("#challenge-button1").toggleClass("comfy");
+  	});
